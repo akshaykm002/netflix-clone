18ed5158c87ee405fdfd76b29c5aae64393a8a6f
akshaykm002 <akshaykmadhu02@gmail.com>
2023-09-27 11:42:09 +0530

    netflix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import requests from './requests';
+import Rows from './components/Rows'
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+      <Rows isPoster={true} title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Rows title="Trending in India" fetchUrl={requests. fetchTrending}/>
+      <Rows title="Top Rated movies" fetchUrl={requests. fetchTopRated}/>
+      <Rows title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Rows title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Rows title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>  
+      <Rows title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies}/>  
+      <Rows title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
+    
+      
     </div>
   );
 }
